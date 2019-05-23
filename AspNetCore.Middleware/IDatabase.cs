729b9ebfe071c7e72aa4b69f221bad4711187c73
Erik Madsen <ekmadsen@comcast.net>
2019-05-23 13:47:40 -0500

    Added IDatabase and SqlDatabase classes.  Intended to be used as a singleton injected into MVC Controller.  Controller will use via using (DbConnection connection = await SqlDatabase.OpenConnection()) { // Execute query. }

---
+++ AspNetCore.Middleware/IDatabase.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Common;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+
+namespace ErikTheCoder.AspNetCore.Middleware
+{
+    public interface IDatabase
+    {
+        [UsedImplicitly]
+        Task<DbConnection> OpenConnection();
+    }
+}
---
+++ AspNetCore.Middleware/SqlDatabase.cs
@@ -0,0 +1,29 @@
+﻿using System.Data.Common;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using ErikTheCoder.AspNetCore.Middleware.Settings;
+using JetBrains.Annotations;
+
+
+namespace ErikTheCoder.AspNetCore.Middleware
+{
+    [UsedImplicitly]
+    public class SqlDatabase : IDatabase
+    {
+        private readonly string _connection;
+
+
+        public SqlDatabase(IAppSettings AppSettings)
+        {
+            _connection = AppSettings.Database;
+        }
+
+
+        public async Task<DbConnection> OpenConnection()
+        {
+            SqlConnection connection = new SqlConnection(_connection);
+            await connection.OpenAsync();
+            return connection;
+        }
+    }
+}
