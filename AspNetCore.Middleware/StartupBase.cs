699eee0c5311d617f1084da4909a730ba7637dd1
Erik Madsen <ekmadsen@comcast.net>
2019-07-04 14:51:18 -0500

    Move IDatabase and SqlDatabase to ErikTheCoder.Data solution.  Fixed bug related to location of appSettings.json files.

--- AspNetCore.Middleware/StartupBase.cs
+++ AspNetCore.Middleware/StartupBase.cs
@@ -14,7 +14,7 @@ namespace ErikTheCoder.AspNetCore.Middleware
         {
             const string environmentalVariableName = "ASPNETCORE_ENVIRONMENT";
             string environment = Environment.GetEnvironmentVariable(environmentalVariableName) ?? Microsoft.AspNetCore.Hosting.EnvironmentName.Development;
-            string directory = Path.GetDirectoryName(typeof(StartupBase).Assembly.Location) ?? string.Empty;
+            string directory = Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory) ?? string.Empty;
             string configurationFile = Path.Combine(directory, "appSettings.json");
             if (!File.Exists(configurationFile)) throw new Exception($"Configuration file not found at {configurationFile}.");
             JObject configuration = JObject.Parse(File.ReadAllText(configurationFile));
