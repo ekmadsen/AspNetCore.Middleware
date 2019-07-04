1889b49909f6a8d2aea26d60b7588f651720a933
Erik Madsen <ekmadsen@comcast.net>
2019-07-04 09:57:35 -0500

    Added abstract StartupBase class with protected ParseConfigurationFile method.

---
+++ AspNetCore.Middleware/StartupBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using JetBrains.Annotations;
+using Newtonsoft.Json.Linq;
+
+
+namespace ErikTheCoder.AspNetCore.Middleware
+{
+    [UsedImplicitly]
+    public abstract class StartupBase
+    {
+        [UsedImplicitly]
+        protected TInterface ParseConfigurationFile<TInterface, TClass>() where TClass : TInterface
+        {
+            const string environmentalVariableName = "ASPNETCORE_ENVIRONMENT";
+            string environment = Environment.GetEnvironmentVariable(environmentalVariableName) ?? Microsoft.AspNetCore.Hosting.EnvironmentName.Development;
+            string directory = Path.GetDirectoryName(typeof(StartupBase).Assembly.Location) ?? string.Empty;
+            string configurationFile = Path.Combine(directory, "appSettings.json");
+            if (!File.Exists(configurationFile)) throw new Exception($"Configuration file not found at {configurationFile}.");
+            JObject configuration = JObject.Parse(File.ReadAllText(configurationFile));
+            return configuration.GetValue(environment).ToObject<TClass>();
+        }
+    }
+}
