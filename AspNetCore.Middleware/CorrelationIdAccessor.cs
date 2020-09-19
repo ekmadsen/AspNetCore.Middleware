df8ffdbfce8319d6c7b327ad7cde83940a242cf2
Erik Madsen <ekmadsen@comcast.net>
2020-09-19 18:03:48 -0500

    Added CorrelationIdAccessor.

---
+++ AspNetCore.Middleware/CorrelationIdAccessor.cs
@@ -0,0 +1,23 @@
+﻿using ErikTheCoder.Logging;
+using System;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Http;
+
+
+namespace ErikTheCoder.AspNetCore.Middleware
+{
+    [UsedImplicitly]
+    public class CorrelationIdAccessor : ICorrelationIdAccessor
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+
+        public CorrelationIdAccessor(IHttpContextAccessor HttpContextAccessor)
+        {
+            _httpContextAccessor = HttpContextAccessor;
+        }
+
+
+        public Guid GetCorrelationId() => _httpContextAccessor.HttpContext.GetCorrelationId();
+    }
+}
